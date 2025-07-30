1f1ea84914d63283f7b5da1931f72c171e3b2033
Louis Cowell-English <louiscowell190@yahoo.co.uk>
2025-07-30 12:52:45 +0000

    Add code for subtraction game

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,8 +14,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 runGame("addition");
 
-}
-)
+});
 
 /**
  * The main game "loop", called when the script is first loaded
@@ -30,6 +29,8 @@ if (gameType === "addition") {
     displayAdditionQuestion(num1, num2);
 } else if (gameType === "multiply") {
     displayMultiplyQuestion(num1, num2);
+}    else if (gameType === "subtract") {
+    displaySubtractQuestion(num1, num2);
 }   else {
     alert(`Unknown game type: ${gameType}`);
     throw `Unknown game type: ${gameType}. Aborting!`;
@@ -64,6 +65,7 @@ function checkAnswer() {
  * directly from the DOM and returns the correct answer.
  */
 function calculateCorrectAnswer() {
+
 let operand1 = parseInt(document.getElementById('operand1').innerText)
 let operand2 = parseInt(document.getElementById('operand2').innerText)
 let operator = document.getElementById('operator').innerText;
@@ -72,7 +74,9 @@ if (operator === "+") {
 return [operand1 + operand2, "addition"];
 } else if (operator === "x") {
      return [operand1 * operand2, "multiply"];
-}    else {
+}   else if (operator === "-") {
+    return [operand1 - operand2, "subtract"];
+}   else {
     alert(`Unimplemented operator ${operator}`);
     throw `Unimplemented operator ${operator}. Aborting!`;
 }
@@ -104,8 +108,12 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = "+";
 }
 
-function displaySubtractQuestion() {
+function displaySubtractQuestion(operand1, operand2) {
     
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-";
+
 }
 
 function displayMultiplyQuestion(operand1, operand2) {
